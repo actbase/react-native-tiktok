5e1b6034d3f43b0ba222e04cb4d1c7d08ede8ee6
Suhan Moon <lunyx@me.com>
2022-10-22 21:30:22 +0900

    Update Tiktok.m

--- ios/Tiktok.m
+++ ios/Tiktok.m
@@ -2,5 +2,4 @@
 
 @interface RCT_EXTERN_MODULE(Tiktok, NSObject)
   RCT_EXTERN_METHOD(auth: (RCTResponseSenderBlock)callback)
-  RCT_EXTERN_METHOD(share: (NSString)path callback: (RCTResponseSenderBlock)callback)
 @end
